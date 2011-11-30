64645f42f6844da7c0d165fc5fac66867434bf09
M. Adam Kendall <linuxbozo@gmail.com>
2011-11-30 09:45:41 -0500

    Proper checking for outdated su binary

--- voodoo/extensions/install_su.sh
+++ voodoo/extensions/install_su.sh
@@ -4,11 +4,14 @@ name='secure su binary for Superuser apk'
 binary_source='/voodoo/extensions/su/su-2.3.6.1-ef'
 apk_source='/voodoo/extensions/su/Superuser.apk'
 binary_dest='/system/xbin/su'
+binary_link='/system/bin/su'
 apk_dest='/system/app/Superuser.apk'
 
 extension_install_su()
 {
+        rm -f $binary_link
 	cp $binary_source $binary_dest
+        ln -sf $binary_dest $binary_link
 	cp $apk_source $apk_dest
 	# make sure it's owned by root
 	chown 0.0 $binary_dest
@@ -27,9 +30,8 @@ if install_condition; then
 	# test if the su binary already exist in xbin
 	if test -u $binary_dest && test -f $apk_dest; then
 		# okay, the su binary exist and is already suid
-		if test $source -nt $dest; then
-
-			# but it's older than ours ! let's updated it
+		if test $binary_source -nt $binary_dest; then
+			# but it's older than ours by datestamp
 			extension_install_su
 		else
 			# ours is the same or older, don't touch it
